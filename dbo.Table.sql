683aeb302f58842e6ae0e799841e09f2437e31fd
mayankraval <mayank.raval8293@gmail.com>
2016-07-28 16:32:46 -0400

    database created and model generated...

---
+++ dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[foodID] INT NOT NULL PRIMARY KEY, 
+    [type] VARCHAR(50) NOT NULL, 
+    [name] VARCHAR(50) NOT NULL, 
+    [description] VARCHAR(50) NOT NULL, 
+    [price] DECIMAL(18, 2) NOT NULL, 
+    [image] VARCHAR(MAX) NOT NULL
+)
